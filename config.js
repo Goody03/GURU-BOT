f25d2633a61902f222011ec17e20f0288ea2a3a4
Goody03 <152481017+Goody03@users.noreply.github.com>
2024-01-14 18:29:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ GOODY BOT 🤖'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'GOODY┃ᴮᴼᵀ' 
+global.author = '@goody03' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
